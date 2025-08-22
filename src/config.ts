5db6ce44995a5b0f0fd1407fd29e6a3fc544f4eb
Sharique <willovateofficial@gmail.com>
2025-08-22 18:18:27 +0530

    update

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 export const baseUrl = process.env.REACT_APP_BASE_URL || "http://localhost:4000";
 // export const frontEndbaseUrl = "https://www.willovateresto.com";
-export const frontEndbaseUrl = "http://localhost:3000"; // For local development, change to production URL when deploying
+export const frontEndbaseUrl = "https://restaurantfrontenddeployment-nine.vercel.app/"; // For local development, change to production URL when deploying
