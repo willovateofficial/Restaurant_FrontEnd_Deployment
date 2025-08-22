1b6d9c8683606f959bc00ac9beaff53ffdaa0307
Sharique <willovateofficial@gmail.com>
2025-08-22 18:07:10 +0530

    update

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
-export const baseUrl ="http://localhost:4000";
+export const baseUrl = process.env.REACT_APP_BASE_URL || "http://localhost:4000";
 // export const frontEndbaseUrl = "https://www.willovateresto.com";
 export const frontEndbaseUrl = "http://localhost:3000"; // For local development, change to production URL when deploying
