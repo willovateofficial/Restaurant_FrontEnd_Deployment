54fbdf0140efccdf147ae3c71a0a2bbfacd171ba
Sharique <willovateofficial@gmail.com>
2025-08-31 14:12:21 +0530

    update

--- src/components/BookDemoPopup.tsx
+++ src/components/BookDemoPopup.tsx
@@ -34,7 +34,7 @@ const BookDemoPopup: React.FC<BookDemoPopupProps> = ({ isOpen, onClose }) => {
         {/* Contact Info */}
         <div className="space-y-3 text-gray-800">
           <div>
-            <span className="font-medium">Phone:</span> +91 9876543210
+            <span className="font-medium">Phone:</span> +91 8767820053
           </div>
           <div>
             <span className="font-medium">Email:</span> admin@willovate.in
